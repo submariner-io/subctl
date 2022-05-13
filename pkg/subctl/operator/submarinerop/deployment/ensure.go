704b0de45b84a16b646919e155bd11e934b813b2
Tom Pantelis <tompantelis@gmail.com>
2022-05-13 08:15:21 -0400

    Use pkg/names from submariner-operator
    Signed-off-by: Tom Pantelis <tompantelis@gmail.com>

--- pkg/subctl/operator/submarinerop/deployment/ensure.go
+++ pkg/subctl/operator/submarinerop/deployment/ensure.go
@@ -23,7 +23,7 @@ import (
 
 	"github.com/pkg/errors"
 	"github.com/submariner-io/subctl/pkg/deployment"
-	"github.com/submariner-io/subctl/pkg/names"
+	"github.com/submariner-io/submariner-operator/pkg/names"
 	appsv1 "k8s.io/api/apps/v1"
 	v1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
