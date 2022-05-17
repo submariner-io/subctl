bf2dd6e8b4d65668d7100b685d4efe26f6e6493f
Tom Pantelis <tompantelis@gmail.com>
2022-05-17 10:24:03 -0400

    Move pkg/subctl/operator/submarinercr under pkg
    Signed-off-by: Tom Pantelis <tompantelis@gmail.com>

--- pkg/deploy/submariner.go
+++ pkg/deploy/submariner.go
@@ -26,7 +26,7 @@ import (
 	"github.com/submariner-io/subctl/internal/constants"
 	"github.com/submariner-io/subctl/pkg/broker"
 	"github.com/submariner-io/subctl/pkg/secret"
-	"github.com/submariner-io/subctl/pkg/subctl/operator/submarinercr"
+	"github.com/submariner-io/subctl/pkg/submarinercr"
 	submariner "github.com/submariner-io/submariner-operator/api/submariner/v1alpha1"
 	"github.com/submariner-io/submariner-operator/pkg/client"
 	"github.com/submariner-io/submariner-operator/pkg/discovery/globalnet"
