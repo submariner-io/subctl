eac01a2d245573a640e12480a442724a3761f53b
Maayan Friedman <maafried@redhat.com>
2022-06-02 14:17:26 +0300

    Add implementation for Azure
    Signed-off-by: Maayan Friedman <maafried@redhat.com>

--- pkg/cloud/prepare/azure.go
+++ pkg/cloud/prepare/azure.go
@@ -42,8 +42,14 @@ func Azure(restConfigProducer *restconfig.Producer, ports *cloud.Ports, config *
 	input := api.PrepareForSubmarinerInput{
 		InternalPorts: []api.PortSpec{
 			{Port: ports.Vxlan, Protocol: "udp"},
-			{Port: ports.Metrics, Protocol: "tcp"},
 		},
+	}
+
+	for i := range ports.Metrics {
+		port := api.PortSpec{
+			Port: ports.Metrics[i], Protocol: "tcp",
+		}
+		input.InternalPorts = append(input.InternalPorts, port)
 	}
 
 	// nolint:wrapcheck // No need to wrap errors here.
