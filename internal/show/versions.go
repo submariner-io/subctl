0c8bd7957deee0320abd101918d6056df77c24c4
Mike Kolesnik <mkolesni@redhat.com>
2022-06-30 11:19:26 +0300

    Use `table` printer in `show` commands
    Use the updated `table` printer in the commands.
    
    The commands were greatly simplified by this change, and the print out
    is much more aligned with `kubectl` behavior and with each other.
    
    Signed-off-by: Mike Kolesnik <mkolesni@redhat.com>

--- internal/show/versions.go
+++ internal/show/versions.go
@@ -20,85 +20,65 @@ package show
 
 import (
 	"context"
-	"fmt"
 
 	"github.com/pkg/errors"
 	"github.com/submariner-io/admiral/pkg/reporter"
 	"github.com/submariner-io/subctl/internal/constants"
+	"github.com/submariner-io/subctl/internal/show/table"
 	"github.com/submariner-io/subctl/pkg/cluster"
-	"github.com/submariner-io/submariner-operator/api/submariner/v1alpha1"
-	submarinerclientset "github.com/submariner-io/submariner-operator/pkg/client/clientset/versioned"
 	"github.com/submariner-io/submariner-operator/pkg/images"
 	"github.com/submariner-io/submariner-operator/pkg/names"
 	apierrors "k8s.io/apimachinery/pkg/api/errors"
 	v1 "k8s.io/apimachinery/pkg/apis/meta/v1"
-	"k8s.io/client-go/kubernetes"
 )
 
-type versionImageInfo struct {
-	component  string
-	repository string
-	version    string
-}
+func getOperatorVersion(clusterInfo *cluster.Info) ([]interface{}, error) {
+	deployments := clusterInfo.ClientProducer.ForKubernetes().AppsV1().Deployments(constants.OperatorNamespace)
 
-func newVersionInfoFrom(repository, component, version string) versionImageInfo {
-	return versionImageInfo{
-		component:  component,
-		repository: repository,
-		version:    version,
-	}
-}
-
-func getSubmarinerVersion(submariner *v1alpha1.Submariner, versions []versionImageInfo) []versionImageInfo {
-	versions = append(versions, newVersionInfoFrom(submariner.Spec.Repository, names.SubmarinerCrName, submariner.Spec.Version))
-	return versions
-}
-
-func getOperatorVersion(clientSet kubernetes.Interface, versions []versionImageInfo) ([]versionImageInfo, error) {
-	operatorConfig, err := clientSet.AppsV1().Deployments(constants.OperatorNamespace).Get(context.TODO(), names.OperatorComponent,
-		v1.GetOptions{})
+	operatorDeployment, err := deployments.Get(context.TODO(), names.OperatorComponent, v1.GetOptions{})
 	if err != nil {
 		if apierrors.IsNotFound(err) {
-			return versions, nil
+			return nil, nil
 		}
 
 		return nil, errors.Wrap(err, "error retrieving Deployment")
 	}
 
-	operatorFullImageStr := operatorConfig.Spec.Template.Spec.Containers[0].Image
-	version, repository := images.ParseOperatorImage(operatorFullImageStr)
-	versions = append(versions, newVersionInfoFrom(repository, names.OperatorComponent, version))
+	version, repository := images.ParseOperatorImage(operatorDeployment.Spec.Template.Spec.Containers[0].Image)
 
-	return versions, nil
+	return []interface{}{names.OperatorComponent, repository, version}, nil
 }
 
-func getServiceDiscoveryVersions(submarinerClient submarinerclientset.Interface, versions []versionImageInfo) ([]versionImageInfo, error) {
-	lighthouseAgentConfig, err := submarinerClient.SubmarinerV1alpha1().ServiceDiscoveries(constants.OperatorNamespace).Get(
-		context.TODO(), names.ServiceDiscoveryCrName, v1.GetOptions{})
+func getServiceDiscoveryVersion(clusterInfo *cluster.Info) ([]interface{}, error) {
+	serviceDiscoveries := clusterInfo.ClientProducer.ForOperator().SubmarinerV1alpha1().ServiceDiscoveries(constants.OperatorNamespace)
+
+	serviceDiscovery, err := serviceDiscoveries.Get(context.TODO(), names.ServiceDiscoveryCrName, v1.GetOptions{})
 	if err != nil {
 		if apierrors.IsNotFound(err) {
-			return versions, nil
+			return nil, nil
 		}
 
 		return nil, errors.Wrap(err, "error retrieving Submariner resource")
 	}
 
-	versions = append(versions, newVersionInfoFrom(lighthouseAgentConfig.Spec.Repository, names.ServiceDiscoveryCrName,
-		lighthouseAgentConfig.Spec.Version))
-
-	return versions, nil
+	return []interface{}{names.ServiceDiscoveryCrName, serviceDiscovery.Spec.Repository, serviceDiscovery.Spec.Version}, nil
 }
 
 func Versions(clusterInfo *cluster.Info, status reporter.Interface) bool {
 	status.Start("Showing versions")
 
-	var versions []versionImageInfo
+	printer := table.Printer{Columns: []table.Column{
+		{Name: "COMPONENT"},
+		{Name: "REPOSITORY"},
+		{Name: "VERSION"},
+	}}
 
-	if clusterInfo.Submariner != nil {
-		versions = getSubmarinerVersion(clusterInfo.Submariner, versions)
+	submariner := clusterInfo.Submariner
+	if submariner != nil {
+		printer.Add(names.SubmarinerCrName, submariner.Spec.Repository, submariner.Spec.Version)
 	}
 
-	versions, err := getOperatorVersion(clusterInfo.ClientProducer.ForKubernetes(), versions)
+	operatorVersion, err := getOperatorVersion(clusterInfo)
 	if err != nil {
 		status.Failure("Unable to get the Operator version", err)
 		status.End()
@@ -106,7 +86,11 @@ func Versions(clusterInfo *cluster.Info, status reporter.Interface) bool {
 		return false
 	}
 
-	versions, err = getServiceDiscoveryVersions(clusterInfo.ClientProducer.ForOperator(), versions)
+	if operatorVersion != nil {
+		printer.Add(operatorVersion...)
+	}
+
+	lighthouseVersion, err := getServiceDiscoveryVersion(clusterInfo)
 	if err != nil {
 		status.Failure("Unable to get the Service-Discovery version", err)
 		status.End()
@@ -114,24 +98,13 @@ func Versions(clusterInfo *cluster.Info, status reporter.Interface) bool {
 		return false
 	}
 
+	if lighthouseVersion != nil {
+		printer.Add(lighthouseVersion...)
+	}
+
 	status.End()
 
-	if len(versions) > 0 {
-		printVersions(versions)
-	}
+	printer.Print()
 
 	return true
-}
-
-func printVersions(versions []versionImageInfo) {
-	template := "%-32.31s%-54.53s%-16.15s\n"
-	fmt.Printf(template, "COMPONENT", "REPOSITORY", "VERSION")
-
-	for _, item := range versions {
-		fmt.Printf(
-			template,
-			item.component,
-			item.repository,
-			item.version)
-	}
 }
