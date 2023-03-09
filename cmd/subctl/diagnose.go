7e372328a681a0622c17268727f10cdc5282a8f9
Sridhar Gaddam <sgaddam@redhat.com>
2023-03-09 17:49:51 +0530

    Include image-override as a firewall subcmd
    Currently the image-override flag was added to the main
    subctl diagnose firewall command and was hence ignored
    when it was passed to the sub-command like intra-cluster,
    inter-cluster etc. This PR fixes it.
    
    Usage:
    subctl diagnose firewall intra-cluster --image-override=...
    
    Partially fixes: https://github.com/submariner-io/subctl/issues/597
    Signed-off-by: Sridhar Gaddam <sgaddam@redhat.com>
    (cherry picked from commit 9087839acc978377f6498fd6bc68fc5a41a6e6b4)
    
    Conflicts:
       cmd/subctl/diagnose.go

--- cmd/subctl/diagnose.go
+++ cmd/subctl/diagnose.go
@@ -177,7 +177,6 @@ func addDiagnoseSubCommands() {
 	diagnose.AddKubeProxyImageOverrideFlag(diagnoseKubeProxyModeCmd.Flags())
 	diagnoseCmd.AddCommand(diagnoseKubeProxyModeCmd)
 	diagnoseCmd.AddCommand(diagnoseAllCmd)
-	diagnose.AddFirewallImageOverrideFlag(diagnoseFirewallCmd.Flags())
 	diagnoseCmd.AddCommand(diagnoseFirewallCmd)
 	diagnoseCmd.AddCommand(diagnoseServiceDiscoveryCmd)
 }
@@ -190,6 +189,9 @@ func addDiagnoseFirewallSubCommands() {
 	diagnoseFirewallNatDiscoveryRestConfigProducer.SetupFlags(diagnoseFirewallNatDiscovery.Flags())
 	addDiagnoseFWConfigFlags(diagnoseFirewallNatDiscovery)
 
+	diagnose.AddFirewallImageOverrideFlag(diagnoseFirewallVxLANCmd.Flags())
+	diagnose.AddFirewallImageOverrideFlag(diagnoseFirewallTunnelCmd.Flags())
+	diagnose.AddFirewallImageOverrideFlag(diagnoseFirewallNatDiscovery.Flags())
 	diagnoseFirewallCmd.AddCommand(diagnoseFirewallMetricsCmd)
 	diagnoseFirewallCmd.AddCommand(diagnoseFirewallVxLANCmd)
 	diagnoseFirewallCmd.AddCommand(diagnoseFirewallTunnelCmd)
